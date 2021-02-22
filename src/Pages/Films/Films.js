65e3bc21ee6102c26e9568ab3cb530198fc346cf
Israel Martinez Vargas <israel.martinez.vargas@gmail.com>
2021-02-21 21:27:57 -0800

    Removing assets and app container style created

---
+++ src/Pages/Films/Films.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Films.css';
+
+const Films = () => {
+  return (
+    <div className="Films-content-screen">
+      Films
+    </div>
+  )
+}
+
+export default Films;
