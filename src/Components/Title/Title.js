b15ac4c710b031e2433c4b07e497c41b112dabbf
Israel Martinez Vargas <israel.martinez.vargas@gmail.com>
2021-02-21 22:24:10 -0800

    Title component created and style added

---
+++ src/Components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Title.css';
+
+const Title = ({ title }) => {
+  return (
+    <h2 className="Title">
+      {title}
+    </h2>
+  )
+}
+
+export default Title;
