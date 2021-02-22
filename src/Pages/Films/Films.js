e12d681e2e21641d21aac01b000966c9073620de
Israel Martinez Vargas <israel.martinez.vargas@gmail.com>
2021-02-21 22:26:07 -0800

    Removing fonts from app css && and using Title and List components

--- src/Pages/Films/Films.js
+++ src/Pages/Films/Films.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Title from '../../Components/Title/Title';
 import './Films.css';
+import List from '../../Components/Films/List';
 
 const Films = () => {
   return (
     <div className="Films-content-screen">
-      Films
+      <Title title="Films" />
+      <List />
     </div>
   )
 }
