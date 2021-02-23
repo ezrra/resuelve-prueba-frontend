e003eca59fbd78d9305a6c59ab63f8d5353d152c
Israel Martinez Vargas <israel.martinez.vargas@gmail.com>
2021-02-22 22:47:07 -0800

    Displaying title and producer of the film

--- src/Components/Film/index.js
+++ src/Components/Film/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Film.css';
 import placeholderImage from '../../assets/images/film-poster-placeholder.png';
 
-const Film = () => {
+const Film = ({ film }) => {
+  const { title, producer } = film;
+
   return (
     <div className="Film-content">
       <img className="Film-image" src={placeholderImage} alt="Film" />
-      <h4 className="Film-title">Castle in the Sky</h4>
-      <h5 className="Film-director">Hayao Miyazaki</h5>
+      <h4 className="Film-title">{title}</h4>
+      <h5 className="Film-director">{producer}</h5>
     </div>
   )
 }
