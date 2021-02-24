16d48e449ebbe4da2f0aaeb237af5622de72e034
Israel Martinez Vargas <israel.martinez.vargas@gmail.com>
2021-02-23 18:30:38 -0800

    Keydown handle created: pressing down, up or enter. Selecting suggestions from the suggestion list. suggestions list  style updated

--- src/Components/SearchField/index.js
+++ src/Components/SearchField/index.js
@@ -1,33 +1,73 @@
 import React, { useState } from 'react';
 import './SearchField.css';
 import i18n from '../../i18n';
+import { filterArrayByText } from '../utils';
+
+const PRESS_ENTER_CODE = 13;
+const PRESS_DOWN_CODE = 40;
+const PRESS_UP_CODE = 38;
 
 const SearchField = ({ suggestions }) => {
   const [search, setSearch] = useState('');
   const [filteredSuggestions, setFilteredSuggestions] = useState([]);
   const [showSuggestions, setShowSuggestions] = useState(false);
+  const [activeSuggestion, setActiveSuggestion] = useState(0);
 
   const Suggestions = () => {
-    const handleSuggestionSelected = (e) => {
+    const handleSuggestionSelected = (event) => {
       setShowSuggestions(false);
-      setSearch(e.currentTarget.innerText);
+      setSearch(event.currentTarget.innerText);
     }
 
     if (!showSuggestions || suggestions.length === 0 || filteredSuggestions.length === 0) {
       return null;
     }
 
+    const classNameSuggestion = (key) => {
+      if (activeSuggestion === key) {
+        return 'Suggestion-active';
+      }
+
+      return null;
+    };
+
     return (
       <ul className="Suggestion-list">
         {filteredSuggestions.map((suggestion, key) => (
-          <li onClick={handleSuggestionSelected} key={key}>{suggestion}</li>
+          <li
+            className={classNameSuggestion(key)}
+            onClick={handleSuggestionSelected}
+            key={key}
+          >
+            {suggestion}
+          </li>
         ))}
       </ul>
     );
-  }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.keyCode === PRESS_DOWN_CODE
+      && activeSuggestion !== filteredSuggestions.length) {
+      setActiveSuggestion(activeSuggestion + 1);
+    }
+
+    if (event.keyCode === PRESS_UP_CODE && activeSuggestion >= 0) {
+      setActiveSuggestion(activeSuggestion - 1);
+    }
+
+    if (event.keyCode === PRESS_ENTER_CODE) {
+      setShowSuggestions(false);
+      const selectedSuggestion = filteredSuggestions[activeSuggestion];
+
+      if (selectedSuggestion) {
+        setSearch(selectedSuggestion);
+      }
+    }
   };
 
   const handleChange = (event) => {
@@ -37,15 +77,17 @@ const SearchField = ({ suggestions }) => {
 
     setSearch(event.target.value);
 
-    const filteredSuggestions =
-      suggestions.filter(suggestion => new RegExp(textInput, 'i').test(suggestion));
+    const filteredSuggestions = filterArrayByText({ array: suggestions, text: textInput });
 
     setFilteredSuggestions(filteredSuggestions);
+
+    setActiveSuggestion(0);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <input
+        onKeyDown={handleKeyDown}
         className="Search-field"
         onChange={handleChange}
         placeholder={i18n.searchField.search}
